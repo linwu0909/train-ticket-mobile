69fdea3fa02c11b09a7a1a51dc0cfec3d48677f6
linjingwu <541814469@qq.com>
2024-07-08 14:38:03 +0800

    提交备注

---
+++ src/index/App.jsx
@@ -0,0 +1,61 @@
+import React, { useCallback, useMemo } from 'react';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux'
+import './App.css';
+
+import Header from '../common/Header';
+import DepartDate from './DepartDate';
+import HighSpeed from './HighSpeed';
+import Journey from './Journey';
+import Submit from './Submit';
+
+import CitySelector from '../common/CitySelector';
+
+import { exchangeFromTo, hideCitySelector, showCitySelector, fetchCityData } from './actions'
+function App(props) {
+  const {from, to, isCitySelectorVisible, cityData, isLoadingCityData,dispatch} = props;
+  const onBack = useCallback(() => {
+    window.history.back()
+  }, [])
+  const cbs = useMemo(() => {
+    return bindActionCreators({
+      exchangeFromTo,
+      showCitySelector,
+      hideCitySelector,
+    }, dispatch)
+  }, [])
+
+  const citySelectorCbs = useMemo(() => {
+    return bindActionCreators({
+      onBack: hideCitySelector,
+      fetchCityData
+    }, dispatch)
+  }, [])
+
+  return (
+      <div className="App">
+          <div className='header-wrapper'>
+              <Header title="火车票" onBack={onBack}/>
+          </div>
+          <form className='form'>
+              <Journey from={from} to={to} {...cbs}/>
+              <DepartDate/>
+              <HighSpeed/>
+              <Submit/>
+          </form>
+          <CitySelector
+          show={isCitySelectorVisible} cityData={cityData} 
+          isLoading={isLoadingCityData}
+          {...citySelectorCbs}/>
+      </div>
+  );
+}
+
+export default connect(
+  function mapStateToProps(state) {
+    return state
+  },
+  function mapDispatchToProps(dispatch) {
+    return { dispatch }
+  }
+)(App);
